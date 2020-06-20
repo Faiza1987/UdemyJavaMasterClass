26020e72f4c8d1eca24427cb8a2edf8cae3e8c65
Faiza Ahsan <faiza.ahsan1222@gmail.com>
2020-06-20 13:47:22 -0700

    Inheritance challenge

---
+++ src/udemyJavaMasterclassAssignmentsInheritance/Main.java
@@ -0,0 +1,13 @@
+package udemyJavaMasterclassAssignmentsInheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Outlander outlander = new Outlander(36);
+		outlander.accelerate(30);
+		outlander.accelerate(20);
+
+	}
+
+}
