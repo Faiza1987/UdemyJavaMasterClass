4272dae1692da0eba344de78128395a200817c98
Faiza Ahsan <faiza.ahsan1222@gmail.com>
2020-06-29 22:08:47 -0700

    Polymorphism challenge

---
+++ src/udemyJavaMasterclassPolymorphism/Holden.java
@@ -0,0 +1,22 @@
+package udemyJavaMasterclassPolymorphism;
+
+public class Holden extends Car{
+	public Holden(String name, int cylinders) {
+		super(name, cylinders);
+	}
+
+	@Override
+	public String startEngine() {
+		return getClass().getSimpleName() + " -> startEngine()";
+	}
+
+	@Override
+	public String accelerate() {
+		return getClass().getSimpleName() + " -> accelerate()";
+	}
+
+	@Override
+	public String brake() {
+		return  getClass().getSimpleName() + " -> brake()";
+	}
+}
