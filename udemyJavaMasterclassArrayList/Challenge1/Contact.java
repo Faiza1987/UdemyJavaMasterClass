26ee012dfc6660eead34f7e6f238d0923248b937
Ahsan Farooq <ahsan.farooq@yahoo.com>
2020-07-18 16:44:18 -0700

    ArrayList challenge

--- udemyJavaMasterclassArrayList/Challenge1/Contact.java
+++ udemyJavaMasterclassArrayList/Challenge1/Contact.java
@@ -13,16 +13,12 @@ public class Contact {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
+	
+	public static Contact createContact(String name, String phoneNumber){
+		return new Contact(name, phoneNumber);
 	}
 	
 	
